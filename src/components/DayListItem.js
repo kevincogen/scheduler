5ae2577ef5f3931b532ecb9df5e00b2cc0af78e2
Kevin Cogen <k.cogen@gmail.com>
2023-06-02 12:26:29 -0400

    DayList - DayListItem complete components

---
+++ src/components/DayListItem.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
+// props.name 
+// props.spots 
+// props.selected
+// props.DaySet
+export default function DayListItem(props) {
+
+  let listClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": (props.spots === 0),
+  });
+
+  const formatSpots = (spots) => {
+    
+    if (spots > 1) return `${props.spots} spots remaining`;
+    if (spots === 1) return `1 spot remaining`;
+    if (spots < 1) return `no spots remaining`; 
+
+  }
+
+  return (
+    <li className={listClass} onClick={() => {props.setDay(props.name)}} >
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
